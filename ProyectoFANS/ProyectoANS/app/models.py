d2c9a05f98cfaa9687098bdc764d25a1ecb3f0b6
Osiris30 <andradeivania16@gmail.com>
2024-06-23 22:46:43 -0600

    modelos

--- ProyectoFANS/ProyectoANS/app/models.py
+++ ProyectoFANS/ProyectoANS/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class ExerciseHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise_type = models.CharField(max_length=50)
+    input_data = models.TextField()
+    result = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.exercise_type} - {self.created_at}"
