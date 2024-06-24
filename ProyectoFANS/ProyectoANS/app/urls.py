863a224030ce90681b81fe009094de3cb5563158
Osiris30 <andradeivania16@gmail.com>
2024-06-23 22:45:56 -0600

    .

--- ProyectoFANS/ProyectoANS/app/urls.py
+++ ProyectoFANS/ProyectoANS/app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/', views.user_register, name='register'),
     path('logout/', views.user_logout, name='logout'),
     path('perfil/', views.view_profile , name='profile'),
+    path('history/', views.view_history, name='history'),
     # RUTAS DE DIRECCIONAMIENTO DE LA CALCULADORA
     path('hermite/', views.hermite, name='hermite'),
     path('runge-kutta/', views.runge_kutta, name='runge_kutta'),
